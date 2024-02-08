65c8cc75d9d8f4800190e80180675bbec588b6e4
RojaMonseny <osorioboteroisabela@gmail.com>
2024-02-08 11:22:06 -0500

    Modificado About

--- Peli/views.py
+++ Peli/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse('<h1>Welcome To Home Page</h1>')
     return render(request, 'home.html', {'name': "Isabela Osorio Botero"})
+def about(request):
+    #return HttpResponse('<h1>Welcome To Home Page</h1>')
+    return render(request, 'home.html', {'name': "Isabela Osorio Botero"})
